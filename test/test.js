0d68daee0ee11279f0ca85b9e2dabe3680711ee0
dongkai1993 <dongkai@wilddog.com>
2016-01-26 18:17:45 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -25,7 +25,7 @@
 // var Wilddog = require('wilddog');
 
 describe('API',function(){
-    this.timeout(10000);
+    this.timeout(20000);
 describe('basic', function() {
     it('new a wildodg()', function() {
         var ref = new Wilddog('https://test123.wilddogio.com');
